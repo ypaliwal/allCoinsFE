4ad97a6ef3c7e76d28bbaac225569716b264f9a8
Yash Paliwal <yash@roverparking.com>
2017-04-23 08:22:52 -0400

    status page finalized

--- src/app/router_views/status/status.component.ts
+++ src/app/router_views/status/status.component.ts
@@ -10,15 +10,20 @@ import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable} from '
 })
 export class StatusComponent implements OnInit {
 
-  private mktcapWeightedRatio;
   private mktcapWeightedRatioTitles: any[] = [];
   private mktcapWeightedRatioValues: any[] = [];
 
   private highRiskRatio;
+  private highRiskRatioTitles: any[] = [];
+  private highRiskRatioValues: any[] = [];
 
   private midRiskRatio;
+  private midRiskRatioTitles: any[] = [];
+  private midRiskRatioValues: any[] = [];
 
   private lowRiskRatio;
+  private lowRiskRatioTitles: any[] = [];
+  private lowRiskRatioValues: any[] = [];
 
   constructor(private _af: AngularFire,
               private _router: Router) { }
@@ -29,25 +34,69 @@ export class StatusComponent implements OnInit {
         // console.log(result);
 
         this.formatMktcapWeightedRatio(result.ticker_new_value);
+        this.formatHighRiskRatio(result.ai_ticker_high_risk);
+        this.formatMidRiskRatio(result.ai_ticker_medium_risk);
+        this.formatLowRiskRatio(result.ai_ticker_low_risk);
       });
   }
 
   formatMktcapWeightedRatio(dataObj) {
 
     for(var i = 0; i < dataObj.length; i++) {
-      console.log(dataObj[i]);
+      // console.log(dataObj[i]);
       
       var ccyName = Object.keys(dataObj[i])[0];
       var ccyNameFinal = ccyName.charAt(0).toUpperCase() + ccyName.slice(1);
       
-      console.log(ccyNameFinal);
-      console.log(dataObj[i][ccyName]);
+      // console.log(ccyNameFinal);
+      // console.log(dataObj[i][ccyName]);
 
-      this.mktcapWeightedRatioValues.push(dataObj[i][ccyName]);
       this.mktcapWeightedRatioTitles.push(ccyNameFinal);
+      this.mktcapWeightedRatioValues.push(parseFloat(dataObj[i][ccyName]).toFixed(2));
+      
     }
     console.log(this.mktcapWeightedRatioValues);
     console.log(this.mktcapWeightedRatioTitles);
+  }
+
+  formatHighRiskRatio(dataObj) {
+    // console.log(dataObj);
+
+    for(var key in dataObj) {
+      var keyNameFinal = key.charAt(0).toUpperCase() + key.slice(1);
+      this.highRiskRatioTitles.push(keyNameFinal);
+      this.highRiskRatioValues.push(parseFloat(dataObj[key]).toFixed(2)); 
+      
+    }
+
+    console.log(this.highRiskRatioTitles);
+    console.log(this.highRiskRatioValues);
+  }
+
+  formatMidRiskRatio(dataObj) {
+    // console.log(dataObj);
+
+    for(var key in dataObj) {
+      var keyNameFinal = key.charAt(0).toUpperCase() + key.slice(1);
+      this.midRiskRatioTitles.push(keyNameFinal);
+      this.midRiskRatioValues.push(parseFloat(dataObj[key]).toFixed(2)); 
+    }
+
+    console.log(this.midRiskRatioTitles);
+    console.log(this.midRiskRatioValues);
+  }
+
+  formatLowRiskRatio(dataObj) {
+    // console.log(dataObj);
+
+    for(var key in dataObj) {
+      var keyNameFinal = key.charAt(0).toUpperCase() + key.slice(1);
+      this.lowRiskRatioTitles.push(keyNameFinal);
+      this.lowRiskRatioValues.push(parseFloat(dataObj[key]).toFixed(2)); 
+    }
+
+    console.log(this.lowRiskRatioTitles);
+    console.log(this.lowRiskRatioValues);
   }
 
 }
